9998b5a06ca817de9c04894ce47d3941d9e23eac
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-15 14:28:46 +0200

    added a viewset for admin crud

--- apps/users/views/users.py
+++ apps/users/views/users.py
@@ -1,24 +1,29 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from django.utils.crypto import get_random_string
-from ..serializers.users import AdminCreateUserSerializer
+from ..serializers.users import AdminCreateUserSerializer, UserSerializer
 from ..models import User
 from apps.core.tasks import send_email_task
 
-class AdminCreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = AdminCreateUserSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    CRUD ViewSet for Users.
+    Only accessible by Admins.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
     permission_classes = [permissions.IsAdminUser]
 
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return AdminCreateUserSerializer
+        return UserSerializer
+
     def perform_create(self, serializer):
-        # Generate random temp password
         temp_password = get_random_string(12)
         user = serializer.save()
         user.set_password(temp_password)
         user.requires_password_change = True
-        # Role is handled by serializer or model default
         user.save()
 
-        # Send Welcome Email
         send_email_task.delay(
             user.email,
             "Welcome to Sky Marshal - Account Created",
