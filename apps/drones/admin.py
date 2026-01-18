3fb0e9c091c76d3fcc170e90651cd5d8c1c4ad2e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-18 15:49:19 +0200

    chore: register DroneStatus and GPSLocation models in the admin interface

--- apps/drones/admin.py
+++ apps/drones/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Drone, Telemetry
+from .models import Drone, DroneStatus, GPSLocation
 
 @admin.register(Drone)
 class DroneAdmin(admin.ModelAdmin):
@@ -7,8 +7,14 @@ class DroneAdmin(admin.ModelAdmin):
     list_filter = ('status', 'model_type')
     search_fields = ('serial_number', 'model_type')
 
-@admin.register(Telemetry)
-class TelemetryAdmin(admin.ModelAdmin):
-    list_display = ('drone', 'timestamp', 'latitude', 'longitude', 'battery_percentage', 'speed')
+@admin.register(DroneStatus)
+class DroneStatusAdmin(admin.ModelAdmin):
+    list_display = ('drone', 'battery_level', 'signal_strength', 'status', 'updated_at')
+    list_filter = ('status', 'drone')
+    search_fields = ('drone__name', 'drone__drone_id')
+
+@admin.register(GPSLocation)
+class GPSLocationAdmin(admin.ModelAdmin):
+    list_display = ('drone', 'timestamp', 'latitude', 'longitude', 'altitude')
     list_filter = ('drone', 'timestamp')
     date_hierarchy = 'timestamp'
