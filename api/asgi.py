fae04e875e4a2528531f633883e90fe354e431a4
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-18 13:26:20 +0200

    chore: update ASGI configuration to support WebSocket routing

--- api/asgi.py
+++ api/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-application = get_asgi_application()
+# Import routing after settings are configured
+from apps.notifications.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
