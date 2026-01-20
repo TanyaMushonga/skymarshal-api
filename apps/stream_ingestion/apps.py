13954b0e8016b8b637975eab287dacfa4cbc831b
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-20 15:54:21 +0200

    registering apps

--- apps/stream_ingestion/apps.py
+++ apps/stream_ingestion/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 
-
 class StreamIngestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.stream_ingestion'
-
+    verbose_name = 'Stream Ingestion'
+    
     def ready(self):
-        import apps.stream_ingestion.signals  # noqa
+        """Import signals when app is ready"""
+        import apps.stream_ingestion.signals
