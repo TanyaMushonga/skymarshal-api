c29e000e433f0499fb619bfb92cf9458acf2b2b8
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-21 15:35:57 +0200

    violation app

---
+++ apps/violations/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class ViolationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.violations'
+
+    def ready(self):
+        import apps.violations.signals
