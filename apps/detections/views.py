71628482ba9242e8eff263a6e5ad6b0cb24a0f6f
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-01-21 17:03:00 +0200

    updated detection views

---
+++ apps/detections/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from apps.core.pagination import StandardResultsSetPagination
+from .models import Detection
+from .serializers import DetectionSerializer
+
+class DetectionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Detection.objects.all().order_by('-timestamp')
+    serializer_class = DetectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['vehicle_type', 'drone__drone_id', 'patrol']
+    search_fields = ['license_plate', 'track_id']
+    ordering_fields = ['timestamp', 'speed']
