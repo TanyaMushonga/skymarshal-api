659ad09ca6d9f472002a1fb2987e893d9ab4093e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-02-21 23:00:17 +0200

    feat: Implement video stream simulation via Celery and add WebSocket consumers for live frame delivery.

--- apps/patrols/apps.py
+++ apps/patrols/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class PatrolsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.patrols'
+
+    def ready(self):
+        import apps.patrols.signals
