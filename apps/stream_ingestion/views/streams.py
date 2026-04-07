eaff13f411471852d6f8920790105407be9d291e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-04-07 15:39:18 +0200

    feat: import permissions from rest_framework in streams viewset

--- apps/stream_ingestion/views/streams.py
+++ apps/stream_ingestion/views/streams.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, filters
+from rest_framework import viewsets, status, filters, permissions
 from apps.core.pagination import StandardResultsSetPagination
 from rest_framework.decorators import action
 from rest_framework.response import Response
