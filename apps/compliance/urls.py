f2dde5fcb65118a224da804f886654b7ef5e477c
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2026-04-09 22:59:23 +0200

    feat: introduce stream_mode to VideoStream to differentiate between LIVE and SIMULATED ingestion sources

--- apps/compliance/urls.py
+++ apps/compliance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LotteryViewSet
+from .views import LotteryViewSet, ComplianceScoreViewSet
 
 router = DefaultRouter()
 router.register(r'lotteries', LotteryViewSet, basename='lottery')
+router.register(r'scores', ComplianceScoreViewSet, basename='compliance-score')
 
 urlpatterns = [
     path('', include(router.urls)),
